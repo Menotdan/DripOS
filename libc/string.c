0d34227e9c5cc80723481fcba4d9f5d5e0538b44
Your Name <you@example.com>
2019-02-25 10:17:21 -0500

    update floppy drivers

--- libc/string.c
+++ libc/string.c
@@ -43,7 +43,7 @@ void hex_to_ascii(int n, char str[]) {
     append(str, 'x');
     char zeros = 0;
 
-    s32 tmp;
+    int32_t tmp;
     int i;
     for (i = 28; i > 0; i -= 4) {
         tmp = (n >> i) & 0xF;
