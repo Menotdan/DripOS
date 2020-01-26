c1cd67bd0ef2c3f164f7b388b6ebfdd7b1016d63
Menotdan <no>
2020-01-25 20:21:10 -0500

    remove printing on interrupts

--- cpu/isr.c
+++ cpu/isr.c
@@ -143,13 +143,6 @@ void register_interrupt_handler(uint8_t n, isr_t handler) {
 }
 
 void irq_handler(registers_t *r) {
-    sprint("\nwut");
-    /* After every interrupt we need to send an EOI to the PICs
-     * or they will not send another interrupt again */
-
-    if (r->int_no >= 40) port_byte_out(0xA0, 0x20); /* slave */
-    port_byte_out(0x20, 0x20); /* master */
-
     /* Handle the interrupt in a more modular way */
     if (interrupt_handlers[r->int_no] != 0) {
         handler = interrupt_handlers[r->int_no];
@@ -170,6 +163,11 @@ void irq_handler(registers_t *r) {
             kprint("");
         }
     }
+    /* After every interrupt we need to send an EOI to the PICs
+     * or they will not send another interrupt again */
+
+    if (r->int_no >= 40) port_byte_out(0xA0, 0x20); /* slave */
+    port_byte_out(0x20, 0x20); /* master */
 }
 
 void irq_install() {
