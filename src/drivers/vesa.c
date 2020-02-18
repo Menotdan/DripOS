0bdaa0f1bb39c0d5429782065aecca798337a50d
Menotdan <aaron.rex08@gmail.com>
2020-02-18 16:12:24 -0500

    Lock VESA for SMP

--- src/drivers/vesa.c
+++ src/drivers/vesa.c
@@ -2,9 +2,11 @@
 #include "mm/vmm.h"
 #include "klibc/stdlib.h"
 #include "klibc/string.h"
+#include "klibc/lock.h"
 #include "drivers/serial.h"
 
 vesa_info_t vesa_display_info;
+lock_t vesa_lock = 0;
 
 void init_vesa(multiboot_info_t *mb) {
     /* Size info */
@@ -19,7 +21,6 @@ void init_vesa(multiboot_info_t *mb) {
     vesa_display_info.blue_shift = mb->framebuffer_blue_field_position;
     /* Framebuffer address */
     vesa_display_info.framebuffer = (uint32_t *) kcalloc(vesa_display_info.framebuffer_size);
-    sprintf("\nbuf: %lx", vesa_display_info.framebuffer);
     vesa_display_info.actual_framebuffer = (uint32_t *) mb->framebuffer_addr;
     vmm_map((void *) mb->framebuffer_addr, (void *) mb->framebuffer_addr, 
         (vesa_display_info.framebuffer_size + 0x1000 - 1) / 0x1000, 
@@ -28,27 +29,42 @@ void init_vesa(multiboot_info_t *mb) {
 }
 
 void put_pixel(uint64_t x, uint64_t y, color_t color) {
+    spinlock_lock(&vesa_lock);
     uint32_t color_dat = (color.r << vesa_display_info.red_shift) |
         (color.g << vesa_display_info.green_shift) |
         (color.b << vesa_display_info.blue_shift);
     
     uint64_t offset = (y * vesa_display_info.pitch) + (x * 4);
     *(uint32_t *) ((uint64_t) vesa_display_info.framebuffer + offset) = color_dat;
+    spinlock_unlock(&vesa_lock);
 }
 
 void render_font(uint8_t font[128][8], char c, uint64_t x, uint64_t y, color_t fg, color_t bg) {
+    spinlock_lock(&vesa_lock);
     for (uint8_t iy = 0; iy < 8; iy++) {
         for (uint8_t ix = 0; ix < 8; ix++) {
             if ((font[(uint8_t) c][iy] >> ix) & 1) {
-                put_pixel((uint64_t) ix + x, (uint64_t) iy + y, fg);
+                uint32_t color_dat = (fg.r << vesa_display_info.red_shift) |
+                    (fg.g << vesa_display_info.green_shift) |
+                    (fg.b << vesa_display_info.blue_shift);
+                
+                uint64_t offset = ((iy + y) * vesa_display_info.pitch) + ((ix + x) * 4);
+                *(uint32_t *) ((uint64_t) vesa_display_info.framebuffer + offset) = color_dat;
             } else {
-                put_pixel((uint64_t) ix + x, (uint64_t) iy + y, bg);
+                uint32_t color_dat = (bg.r << vesa_display_info.red_shift) |
+                    (bg.g << vesa_display_info.green_shift) |
+                    (bg.b << vesa_display_info.blue_shift);
+                
+                uint64_t offset = ((iy + y) * vesa_display_info.pitch) + ((ix + x) * 4);
+                *(uint32_t *) ((uint64_t) vesa_display_info.framebuffer + offset) = color_dat;
             }
         }
     }
+    spinlock_unlock(&vesa_lock);
 }
 
 void vesa_scroll(uint64_t rows_shift) {
+    spinlock_lock(&vesa_lock);
     uint64_t buf_pos = (uint64_t) vesa_display_info.framebuffer;
     /* Do the copy */
     memcpy32((uint32_t *) (buf_pos + (vesa_display_info.pitch * rows_shift)), (uint32_t *) (buf_pos), 
@@ -56,13 +72,18 @@ void vesa_scroll(uint64_t rows_shift) {
     /* Do the clear */
     memset32((uint32_t *) (buf_pos + vesa_display_info.framebuffer_size - (vesa_display_info.pitch * rows_shift)),
         0, (vesa_display_info.pitch * rows_shift) / 4);
+    spinlock_unlock(&vesa_lock);
 }
 
 void flip_buffers() {
+    spinlock_lock(&vesa_lock);
     memcpy32(vesa_display_info.framebuffer, vesa_display_info.actual_framebuffer,
         vesa_display_info.framebuffer_pixels);
+    spinlock_unlock(&vesa_lock);
 }
 
 void clear_buffer() {
+    spinlock_lock(&vesa_lock);
     memset32(vesa_display_info.framebuffer, 0, vesa_display_info.framebuffer_pixels);
+    spinlock_unlock(&vesa_lock);
 }
